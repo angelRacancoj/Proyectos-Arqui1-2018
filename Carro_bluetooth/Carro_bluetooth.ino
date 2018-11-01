810be64ac02c3403c1033cb86e86ff242e5b827f
angelRacancoj <the1999evilrg@gmail.com>
2018-11-01 12:15:02 -0600

    Control Bluethoot - Sigue Lineas
    El auto es capaza ahora de asignarse si es a control remoto o ser siguelias en dos faces:
    -> La primera es semi automatica donde se debe presionar la hacia adelante para que funciones como sigue lineas (Previamente se debe seleccionar modo siguelineas)
    -> Completamente automatico, donde "encender" esta segunda funcion el auto se convierte en completamente siguelineas

--- Carro_bluetooth/Carro_bluetooth.ino
+++ Carro_bluetooth/Carro_bluetooth.ino
@@ -1,5 +1,28 @@
 /**
  * Autor: Misael Saenz Flores
+ * Modificado por: Angel Racancoj
+ *
+ * Este codigo originalmente se dedicaba al manejo de un automovil mediante bluetooth
+ * ha sido optimizado para una respuesta mas rapida
+ *
+ * Las modificaciones principal en el codigo, son un manejo de enciendido y apagado de dos luces
+ * y se ha agregado el manejo en modo sigue lineas indicado con el metodo "Automatico"
+ *
+ * +------------------------------------------------------------------------+
+ * | Descripcion del Automovil:                                             |
+ * | Se utilizo un auto a control remoto con un motor en la parte frontal   |
+ * | encargado del giro y un motor en la parte posterior para la traccion   |
+ * | el restro de atributos como la colocacion de los sensores y luces      |
+ * | queda a criterio de quien construya el auto                            |
+ * +------------------------------------------------------------------------+
+ *
+ * Modulos Utilizados:
+ * Arduino Nano
+ * Sensor Ultrasonico HC-SR04 (Para el control de freno automatico respecto a la distancia)
+ * Modulo de sensor infrarojo MH-sensor
+ * Modulo Bluetooth ZS-040
+ * Modulo de Puente H 
+ *
  * Esta obra está bajo una Licencia Creative Commons 
  * Atribución-NoComercial-SinDerivadas 4.0 Internacional.
  */
@@ -9,7 +32,27 @@
 #define Motor2_1 4 //Pin 15 L293D
 #define Motor2_2 5 //Pin 12 L293D
 
+#define InfraD 6  //Pin entrada sensor infrarojo derecho
+#define InfraI 7  //Pin entrada sensor infrarojo derecho
+
+#define GreenLight 8 //green light
+#define frontLight 9 //Principal Light
+
+#define Trig 10 //Ultrasonic sensor trig port
+#define Echo 11 //Untrasonic sensor echo port
+
+char modoManejo;
 char dato;
+char directo;
+
+int tiempo = 400;
+int tiempo2 = 750; 
+
+int valInfraD = 0;
+int valInfraI = 0;
+
+int blanco = 0;
+int negro = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -18,51 +61,248 @@ void setup() {
   pinMode(Motor2_1, OUTPUT);
   pinMode(Motor2_2, OUTPUT);
 
+  pinMode(InfraD, INPUT);
+  pinMode(InfraI, INPUT);
+
+  pinMode(GreenLight, OUTPUT);
+  pinMode(frontLight, OUTPUT);
+
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+
+  Alto();
+
+  modoManejo = 'x';
+  directo = 'v';
 }
 
 void loop() {
-  if (Serial.available() > 0) {
+  long distance = medirDistancia();
+
+  Serial.print("D");
+  Serial.println(distance);
+  delay(10);
+
+
+  while (Serial.available() > 0) {
     dato = Serial.read();
+    //Serial.print("Dato entrada: ");
+    //Serial.println(dato);
+    switch(dato){
+    case 'x':
+    case 'X':
+      modoManejo = dato;
+      //Serial.print("Modo de automatico: ");
+      //Serial.println(modoManejo);
+      break;
+    case 'V':
+    case 'v':
+      directo = dato;
+      //Serial.print("Modo de automatico 2: ");
+      //Serial.println(directo);
+      break;      
+    }
+
   }
 
-  switch (dato) {
-    case 'w':
-      Adelante();
-      Serial.println("Adelante");
+  switch(modoManejo){
+    //Manual -> Para ser utilizado a control remoto
+  case 'x':
+    Manual(dato);
+    break;
+    //Automatico -> Para ser utilizado como carro sigue lineas
+  case 'X':
+
+    valInfraD = digitalRead(InfraD);
+    valInfraI = digitalRead(InfraI);
+    Automatico(valInfraD, valInfraI, distance);
+    break;
+  }
+}
+
+long medirDistancia(){
+  //Funcion de control del sensor ultra sonico, esta funcion esta unicamente habilitada para modo Sigue lineas
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+
+  long duration = pulseIn(Echo, HIGH);
+
+  return microSecToCm(duration);
+}
+
+long microSecToCm(long microSeconds){
+  return microSeconds/29/2;
+}
+
+/*
+* "Automatico" es la funcion asignada para el modo Siguelineas
+* tiene dos modos de uso en esta modalidad:
+* -> La primera es "semi automatica" donde se debe presionar la hacia adelante para
+*    que funciones como sigue lineas (Previamente se debe seleccionar modo siguelineas)
+* -> Completamente "automatico", donde "encender" esta segunda funcion el auto 
+*    se convierte en completamente siguelineas
+*/
+void Automatico(int derecha, int izquierda, long distance){
+
+  if(directo == 'v'){
+    switch(dato){
+        
+    case'F':
+        /*
+        * "Semi automatico"
+        */
+      if(distance>20){
+        Auto(derecha, izquierda);
+      } 
+      else {
+        Alto();
+      }
       break;
-    case 'a':
-      Serial.println("Izquierda");
-      Derecha();
-      //Izquierda();
-      break;
-    case 'd':
-      Serial.println("Derecha");
-      Izquierda();
-      //Derecha();
-      break;
-    case 'f':
-      Serial.println("Alto");
+    case 'S':
       Alto();
       break;
-    case 's':
-      Serial.println("Reversa");
-      Reversa();
+    case 'W':
+      Serial.println("Front Light On");
+      digitalWrite(frontLight, HIGH);
+      break;
+    case 'w':
+      Serial.println("Front Light Off");
+      digitalWrite(frontLight, LOW);
+      break;
+    case 'U':
+      Serial.println("Green Light On");
+      digitalWrite(GreenLight, HIGH);
+      break;
+    case 'u':
+      Serial.println("Green Light Off");
+      digitalWrite(GreenLight, LOW);
       break;
     default:
       Alto();
       break;
+    }
+  } 
+  else if(directo == 'V'){
+    /*
+    * "Automatico"
+    */
+    if(distance>20){
+      Auto(derecha, izquierda);
+    } 
+    else {
+      Alto();
+    }
+
+    switch(dato){
+    case 'W':
+      Serial.println("Front Light On");
+      digitalWrite(frontLight, HIGH);
+      break;
+    case 'w':
+      Serial.println("Front Light Off");
+      digitalWrite(frontLight, LOW);
+      break;
+    case 'U':
+      Serial.println("Green Light On");
+      digitalWrite(GreenLight, HIGH);
+      break;
+    case 'u':
+      Serial.println("Green Light Off");
+      digitalWrite(GreenLight, LOW);
+      break;
+    }
   }
 
 }
 
+void Auto(int Der, int Izq){
+  if((Der == blanco) && (Izq == blanco)){  //Adelante
+    Adelante();
+    delay(175);
+    Reversa();
+    delay(5);
+  } 
+  else if((Izq == blanco) && (Der == negro)){  //Izquierda
+    IzquierdaRet();
+    delay(tiempo);
+    DerechaAd();
+    delay(tiempo);
+  } 
+  else if ((Izq == negro) && (Der == blanco)){  //Derecha
+    DerechaRet();
+    delay(tiempo);
+    IzquierdaAd();
+    delay(tiempo);
+  } 
+  else {
+    Alto();
+  }
+}
+
+void Manual(char dato){
+  switch (dato) {
+  case 'F':
+    Adelante();
+    break;
+  case 'B':
+    Reversa();
+    break; 
+  case 'L':
+    Izquierda();
+    break;
+  case 'R':
+    Derecha();
+    break;
+  case 'G':
+    IzquierdaAd();
+    break;
+  case 'I':
+    DerechaAd();
+    break;
+  case 'H':
+    IzquierdaRet();
+    break;
+  case 'J':
+    DerechaRet();
+    break;
+  case 'S':
+    Alto();
+    break;
+  case 'W':
+    Serial.println("Front Light On");
+    digitalWrite(frontLight, HIGH);
+    break;
+  case 'w':
+    Serial.println("Front Light Off");
+    digitalWrite(frontLight, LOW);
+    break;
+  case 'U':
+    Serial.println("Green Light On");
+    digitalWrite(GreenLight, HIGH);
+    break;
+  case 'u':
+    Serial.println("Green Light Off");
+    digitalWrite(GreenLight, LOW);
+    break; 
+  default:
+    Alto();
+    break;
+  }
+}
+
 void Adelante() {
+  Serial.println("Adelante");
   digitalWrite(Motor1_1, LOW);
-  digitalWrite(Motor1_2, HIGH);
+  digitalWrite(Motor1_2, LOW);
   digitalWrite(Motor2_2, HIGH);
   digitalWrite(Motor2_1, LOW);
 }
 
 void Derecha() {
+  Serial.println("Derecha");
   digitalWrite(Motor1_1, LOW);
   digitalWrite(Motor1_2, HIGH);
   digitalWrite(Motor2_2, LOW);
@@ -70,13 +310,47 @@ void Derecha() {
 }
 
 void Izquierda() {
+  Serial.println("Izquierda");
+  digitalWrite(Motor1_1, HIGH);
+  digitalWrite(Motor1_2, LOW);
+  digitalWrite(Motor2_2, LOW);
+  digitalWrite(Motor2_1, LOW);
+}
+
+void DerechaAd() {
+  Serial.println("Derecha Adelante");
+  digitalWrite(Motor1_1, LOW);
+  digitalWrite(Motor1_2, HIGH);
   digitalWrite(Motor2_2, HIGH);
   digitalWrite(Motor2_1, LOW);
-  digitalWrite(Motor1_1, LOW);
+}
+
+void IzquierdaAd() {
+  Serial.println("Izquierda Adelante");
+  digitalWrite(Motor1_1, HIGH);
   digitalWrite(Motor1_2, LOW);
+  digitalWrite(Motor2_2, HIGH);
+  digitalWrite(Motor2_1, LOW);
+}
+
+void DerechaRet() {
+  Serial.println("Derecha Retroceso");
+  digitalWrite(Motor1_1, LOW);
+  digitalWrite(Motor1_2, HIGH);
+  digitalWrite(Motor2_2, LOW);
+  digitalWrite(Motor2_1, HIGH);
+}
+
+void IzquierdaRet() {
+  Serial.println("Izquierda Retroceso");
+  digitalWrite(Motor1_1, HIGH);
+  digitalWrite(Motor1_2, LOW);
+  digitalWrite(Motor2_2, LOW);
+  digitalWrite(Motor2_1, HIGH);
 }
 
 void Alto() {
+  Serial.println("Alto");
   digitalWrite(Motor2_2, LOW);
   digitalWrite(Motor2_1, LOW);
   digitalWrite(Motor1_1, LOW);
@@ -84,8 +358,13 @@ void Alto() {
 }
 
 void Reversa() {
-  digitalWrite(Motor1_1, HIGH);
+  Serial.println("Reversa");
+  digitalWrite(Motor1_1, LOW);
   digitalWrite(Motor1_2, LOW);
   digitalWrite(Motor2_2, LOW);
   digitalWrite(Motor2_1, HIGH);
 }
+
+
+
+
